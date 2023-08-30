0ab3f6ef6f677922dc881e297fa4a3e6086aec95
blueprint-12 <blueprint12@daum.net>
2023-08-31 03:48:40 +0900

    feat: 상수 추출
    -[issue] 다른 파일로 분리하지 않으면 페이지 build시, 타입 에러 발생 (fixed)
    -[ref]: https://github.com/vercel/next.js/issues/50870

---
+++ src/app/shared/const.ts
@@ -0,0 +1,18 @@
+export const typesOfItem = [
+  "blouse",
+  "sweater",
+  "hoodie",
+  "dress shirt",
+  "tank top",
+  "cardigan",
+  "trench coat",
+];
+
+export const typesOfPants = [
+  "cargo",
+  "bell-bottoms",
+  "jeans",
+  "jodhpurs",
+  "leggins",
+  "capri",
+];
